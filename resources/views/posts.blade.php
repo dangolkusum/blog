3d87ec396ffbfa4c51bd77672e42cd9b646e7f81
Kusum Dangol <dangolkusum75@gmail.com>
2023-04-22 18:08:13 +0545

    initial commit migration run table created

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<html>
+    <title></title>
+    <body>
+        <h1>my first post</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dicta unde repellat nobis nam sit, aspernatur culpa eaque veniam nostrum repellendus eos quisquam eius qui similique perferendis corporis harum laboriosam!locale_filter_matcheslorem
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni voluptatibus facere eum adipisci, iusto, culpa aut eos vel mollitia tenetur debitis dolore distinctio laborum quo eveniet, ipsam aspernatur quam!
+        </p>
+
+        <h1>my second post</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dicta unde repellat nobis nam sit, aspernatur culpa eaque veniam nostrum repellendus eos quisquam eius qui similique perferendis corporis harum laboriosam!locale_filter_matcheslorem
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni voluptatibus facere eum adipisci, iusto, culpa aut eos vel mollitia tenetur debitis dolore distinctio laborum quo eveniet, ipsam aspernatur quam!
+        </p>
+
+        <h1>my third post</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dicta unde repellat nobis nam sit, aspernatur culpa eaque veniam nostrum repellendus eos quisquam eius qui similique perferendis corporis harum laboriosam!locale_filter_matcheslorem
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni voluptatibus facere eum adipisci, iusto, culpa aut eos vel mollitia tenetur debitis dolore distinctio laborum quo eveniet, ipsam aspernatur quam!
+        </p>
+    </body>
+</html>
