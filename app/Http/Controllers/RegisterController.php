ba22f146a6e8ba03765f0845489a8ddc679360ed
Kusum Dangol <dangolkusum75@gmail.com>
2023-06-27 21:50:17 +0545

    Authentication hashing password error display validations

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -14,14 +15,16 @@ class RegisterController extends Controller
 
     public function store()
     {
-        $attribute = request()->validate([
+        $attributes = request()->validate([
             'name'=>['required','max:255'],
-            'username'=>'required|max:255|min:3',
-            'email'=>['required','email','max:255'],
+            'username'=>'required|max:255|min:3|unique:users,username',
+            'email'=>['required','email','max:255',Rule::unique('users','email')],
             'password'=>'required|min:7|max:255',   
 
         ]);
-        User::create($attribute);
+        $attributes['password']= bcrypt($attributes['password']);
+            User::create($attributes);
+
         
             return redirect('/');
         
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -14,8 +14,12 @@
                 type="text"
                 name="name"
                 id="name" 
+                value="{{old('name')}}"
                 required
             >
+            @error('name')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
             
         <div class="mb-6">
@@ -27,8 +31,12 @@
                 type="text"
                 name="username"
                 id="username" 
+                value="{{old('username')}}"
                 required
             >
+            @error('username')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
@@ -42,6 +50,9 @@
                 id="password" 
                 required
             >
+            @error('password')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
@@ -53,8 +64,12 @@
                 type="email"
                 name="email"
                 id="email" 
+                value="{{old('email')}}"
                 required
             >
+            @error('email')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
